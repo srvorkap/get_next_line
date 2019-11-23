163a425956271b6fda6a7c64f2fb04bdfb6c2b4b
Srdan Vorkapic <srvorkap@e1z3r6p13.42.us.org>
2019-11-22 21:40:06 -0800

    main

--- unsubmitted/main.c
+++ unsubmitted/main.c
@@ -6,7 +6,7 @@
 /*   By: srvorkap <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/09 23:22:55 by srvorkap          #+#    #+#             */
-/*   Updated: 2019/11/09 23:45:43 by srvorkap         ###   ########.fr       */
+/*   Updated: 2019/11/22 21:35:24 by srvorkap         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ int		main(int ac, char **av)
 	int		fd2;
 	char	*line;
 
+	if (ac < 2)
+		printf("\n");
 	fd1 = open(av[1], O_RDONLY);
 	fd2 = open(av[2], O_RDONLY);
 	while (get_next_line(fd1, &line) == 1)
